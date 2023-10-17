ae0db59a8c4d1da977f511779f8c3d41e5e8234a
crayhung <crayhung168@gmail.com>
2023-10-17 10:08:59 +0800

    first commit

---
+++ spring/Xinwu/src/test/java/com/twoway/Xinwu/XinwuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twoway.Xinwu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XinwuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
